906896dba1503075e86012bc7d1631d6a4176dcb
574549756 <574549756@qq.com>
2018-05-08 05:44:28 +0800

    JSONP

---
+++ index.js
@@ -0,0 +1,67 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号可不可以？\nnode server.js 8888 这样是不是舒服点？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+    console.log('含查询字符串的路径\n' + pathWithQuery)
+
+  if (path === '/') {
+    var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8')       //db是拿来存这个100块钱的
+    string = string.replace('&&&amount&&&', amount)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.statusCode = 200
+    response.write('可以，进来了')
+    response.end()
+  } else if (path === '/style.css') {
+    var string = fs.readFileSync('./style.css', 'utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  } else if (path === '/main.js') {
+    var string = fs.readFileSync('./main.js', 'utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+    //上面一坨是请求页面的，重点关注下面一坨
+  } else if (path === '/pay') {
+    var amount = fs.readFileSync('./db', 'utf8')   //db是拿来存这个100块钱的
+    var newAmount = amount - 1
+    fs.writeFileSync('./db', newAmount)
+    response.setHeader('Content-Type', 'application/javascript')
+    response.statusCode = 200
+    response.write(`
+      ${query.callback}.call(undefined,{
+        "success": true,
+        "left": ${newAmount}
+      })
+    `)
+  } else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('找不到对应路径，你需要自行修改 index.js')
+    response.end()
+  }
+
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用洗衣机打开 http://localhost:' + port)
