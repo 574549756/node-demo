ee4d3cd805ccbb69970977dff7fefa8f700bbe39
574549756 <574549756@qq.com>
2018-06-07 22:57:16 +0800

    注册界面初

--- server.js
+++ server.js
@@ -22,18 +22,51 @@ var server = http.createServer(function (request, response) {
     console.log('含查询字符串的路径\n' + pathWithQuery)
 
     if (path === '/') {
-        var string = fs.readFileSync('./index.html', 'utf8')
+        let string = fs.readFileSync('./index.html', 'utf8')
         response.statusCode = 200                                       //第一部分        
         response.setHeader('Content-Type', 'text/html;charset=utf-8')       //第二部分
         response.write(string)                                          //第四部分
         response.end()                                                      //结束
-    } else if (path === '/main.js'){
-        var string = fs.readFileSync('./main.js', 'utf8')
+    } else if (path === '/sign_up' && method === 'GET') {
+        let string = fs.readFileSync('./sign_up.html', 'utf8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(string)
+        response.end()
+    } else if (path === '/sign_up' && method === 'POST') {
+        readBody(request).then((body) => {
+            let strings = body.split('&')
+            let hash = {}
+            strings.forEach((string)=>{
+                let parts = string.split('=')
+                let key = parts[0]
+                let value = parts[1]
+                hash[key] = value
+            })
+            let {email, password, password_confirmation} = hash
+            if(email.indexOf('@') === -1){
+                response.statusCode = 400
+                response.setHeader('Content-Type', 'application/json;charset=utf-8')
+                response.write(`{
+                    "errors": {
+                        "email": "invalid"
+                    }
+                }`)
+            }else if(password !== password_confirmation){
+                response.statusCode = 400
+                response.write('password not match')
+            }else{
+                response.statusCode = 200
+            }
+            response.end()
+        })
+    } else if (path === '/main.js') {
+        let string = fs.readFileSync('./main.js', 'utf8')
         response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
         response.statusCode = 200
         response.write(string)
         response.end()
-    } else if (path === '/xxx' && method === 'GET'){
+    } else if (path === '/xxx' && method === 'GET') {
         response.statusCode = 200
         response.setHeader('Content-type', 'text/json;charset=utf-8')
         response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001')
@@ -59,6 +92,18 @@ var server = http.createServer(function (request, response) {
 
     /******** 代码结束，下面不要看 ************/
 })
+
+function readBody(request) {
+    return new Promise((resolve, reject) => {
+        let body = []
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            resolve(body)
+        });
+    })
+}
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用洗衣机打开 http://localhost:' + port)
